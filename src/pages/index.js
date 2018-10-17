07e871075c1c1b71837a26b46d277006277c1001
Christian <christian@stabmag.com>
2018-10-16 17:38:27 -0700

    fetched data to screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      issues: [],
+    }
+  }
 
-export default IndexPage
+  componentDidMount() {
+    const data = fetch(
+      'https://api.github.com/repos/rails/rails/issues?state=open&sort=comments'
+    ).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+
+    data.then(d => {
+      this.setState({
+        issues: d,
+      })
+    })
+  }
+
+  render() {
+    const issues =
+      this.state.issues &&
+      Object.keys(this.state.issues).map(key => {
+        const issue = this.state.issues[key]
+        return (
+          <div key={key}>{issue.title}</div>
+        )
+      })
+
+    return (
+      <Layout>
+        <h1>Github API Challenge</h1>
+        {issues}
+      </Layout>
+    )
+  }
+}
